96222491e7add355afd7adc886fe08b5d0e23cb7
Sorady <125706196+Sorady@users.noreply.github.com>
2023-07-12 02:10:36 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""
-Module 7-add_item.py that adds all arguments to a Python list, and
-then save them to a file.
-"""
+"""loads from.json file"""
+from os import path
 from sys import argv
 
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__(
-        '6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-    add_file = "add_item.json"
+if path.exists('add_item.json'):
+    obj_json_file = load_from_json_file('add_item.json')
+else:
+    obj_json_file = []
 
-    try:
-        new_list = load_from_json_file(add_file)
-    except FileNotFoundError:
-        new_list = []
-    new_list.extend(argv[1:])
-    save_to_json_file(new_list, add_file)
+for i in range(1, len(argv)):
+    obj_json_file.append(argv[i])
+
+save_to_json_file(obj_json_file, 'add_item.json')
