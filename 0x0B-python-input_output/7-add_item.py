d34d332833f4960ba826e2327c39b06c0278caab
Sorady <125706196+Sorady@users.noreply.github.com>
2023-07-12 02:09:04 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-"""Module for add_item method"""
-import sys
-from os import path
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+"""
+Module 7-add_item.py that adds all arguments to a Python list, and
+then save them to a file.
+"""
+from sys import argv
 
 
-filename = "add_item.json"
-if path.exists(filename):
-    my_list = load_from_json_file(filename)
-else:
-    my_list = []
-my_list.extend(sys.argv[1:])
-save_to_json_file(my_list, filename)
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__(
+        '6-load_from_json_file').load_from_json_file
+
+    add_file = "add_item.json"
+
+    try:
+        new_list = load_from_json_file(add_file)
+    except FileNotFoundError:
+        new_list = []
+    new_list.extend(argv[1:])
+    save_to_json_file(new_list, add_file)
