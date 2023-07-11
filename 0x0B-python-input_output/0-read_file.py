3c8bef8e59133486bd8f36fd4ca8a2735eb4b694
Sorady <125706196+Sorady@users.noreply.github.com>
2023-07-12 02:13:36 +0300

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Module for read_file method"""
-
-
-def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, "r", encoding='utf-8') as f:
-        print(f.read(), end="")
