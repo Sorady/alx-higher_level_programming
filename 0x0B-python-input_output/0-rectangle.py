12493388a7794e0f69d4080744ac0da755324eb4
Sorady <125706196+Sorady@users.noreply.github.com>
2023-07-12 02:17:36 +0300

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/0-rectangle.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""This module contains an empty class Rectangle that defines a rectangle."""
-
-
-class Rectangle:
-    """An empty class Rectangle that defines a rectangle."""
-    pass
