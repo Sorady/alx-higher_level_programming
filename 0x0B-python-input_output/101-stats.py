e91478e9ab2e9903c889322a0a8a97064b4acf65
Sorady <125706196+Sorady@users.noreply.github.com>
2023-07-12 01:53:15 +0300

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,28 +1,43 @@
 #!/usr/bin/python3
-import sys
+"""Module for stdin metrics"""
 
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+if __name__ == "__main__":
+    import sys
 
-try:
-    for line in sys.stdin:
-        line_count += 1
-        tokens = line.split()
-        total_size += int(tokens[-1])
-        try:
-            status_codes[int(tokens[-2])] += 1
-        except KeyError:
-            pass
-        if line_count % 10 == 0:
-            print("File size: {}".format(total_size))
-            for k in sorted(status_codes.keys()):
-                if status_codes[k] != 0:
-                    print("{}: {}".format(k, status_codes[k]))
-        sys.stdout.flush()
+    size = 0
+    status_codes = {}
+    valid_codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
+    count = 0
 
-except KeyboardInterrupt:
-    print("File size: {}".format(total_size))
-    for k in sorted(status_codes.keys()):
-        if status_codes[k] != 0:
-            print("{}: {}".format(k, status_codes[k]))
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print("File size: {}".format(size))
+                for key in sorted(status_codes):
+                    print("{}: {}".format(key, status_codes[key]))
+                count = 1
+            else:
+                count += 1
+            line = line.split()
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print("File size: {}".format(size))
+        for key in sorted(status_codes):
+            print("{}: {}".format(key, status_codes[key]))
+
+    except KeyboardInterrupt:
+        print("File size: {}".format(size))
+        for key in sorted(status_codes):
+            print("{}: {}".format(key, status_codes[key]))
+        raise
