3453be5d1dea1e3a1af4d38063326aba4a2e838d
Sorady <125706196+Sorady@users.noreply.github.com>
2023-07-12 01:47:17 +0300

    Update1 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-"""Script that reads stdin line by line and computes metrics."""
 import sys
 
 status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-status_count = {code: 0 for code in status_codes}
+code_count = {code: 0 for code in status_codes}
 total_size = 0
 line_count = 0
+
+def print_stats():
+    print("File size: {}".format(total_size))
+    for code in sorted(code_count.keys()):
+        if code_count[code]:
+            print("{}: {}".format(code, code_count[code]))
 
 try:
     for line in sys.stdin:
@@ -13,20 +18,15 @@ try:
         data = line.split()
         try:
             status_code = int(data[-2])
+            file_size = int(data[-1])
+            total_size += file_size
             if status_code in status_codes:
-                status_count[status_code] += 1
-            total_size += int(data[-1])
+                code_count[status_code] += 1
         except:
             pass
         if line_count % 10 == 0:
-            print("File size: {}".format(total_size))
-            for code in sorted(status_codes):
-                if status_count[code]:
-                    print("{}: {}".format(code, status_count[code]))
+            print_stats()
+    print_stats()
 except KeyboardInterrupt:
-    pass
-finally:
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes):
-        if status_count[code]:
-            print("{}: {}".format(code, status_count[code]))
+    print_stats()
+    raise
