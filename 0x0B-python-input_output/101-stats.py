601bf009df6a9cbd0656ed4e8fa6304440a0f9d7
Sorady <125706196+Sorady@users.noreply.github.com>
2023-07-13 00:48:23 +0300

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,68 +1,40 @@
 #!/usr/bin/python3
-"""script to read stdin line by line and compute metrics"""
+"""
+reads stdin line by line and computes metrics
+"""
+import sys
 
-import sys  # Import the sys module to access stdin
-import signal  # Import the signal module to handle keyboard interruption
-
-# Initialize a dictionary to store the status codes and their counts
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+file_size = 0
+status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
                 "403": 0, "404": 0, "405": 0, "500": 0}
-# Initialize a variable to store the total file size
-total_size = 0
-# Initialize a variable to store the number of lines read
-line_count = 0
+i = 0
+try:
+    for line in sys.stdin:
+        tokens = line.split()
+        if len(tokens) >= 2:
+            a = i
+            if tokens[-2] in status_tally:
+                status_tally[tokens[-2]] += 1
+                i += 1
+            try:
+                file_size += int(tokens[-1])
+                if a == i:
+                    i += 1
+            except FileNotFoundError:
+                if a == i:
+                    continue
+        if i % 10 == 0:
+            print("File size: {:d}".format(file_size))
+            for key, value in sorted(status_tally.items()):
+                if value:
+                    print("{:s}: {:d}".format(key, value))
+    print("File size: {:d}".format(file_size))
+    for key, value in sorted(status_tally.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
 
-# Define a function to print the statistics
-
-
-def print_stats():
-    """Prints the statistics"""
-
-    # Print the total file size
-    print("File size: {}".format(total_size))
-    # Loop through the status codes in ascending order
-    for code in sorted(status_codes.keys()):
-        # If the count is not zero, print the code and the count
-        if status_codes[code] != 0:
-            print("{}: {}".format(code, status_codes[code]))
-
-# Define a function to handle keyboard interruption
-
-
-def signal_handler(sig, frame):
-    """Handles keyboard interruption"""
-    # Print the statistics before exiting
-    print_stats()
-    sys.exit(0)
-
-# Register the signal handler for SIGINT (CTRL + C)
-
-
-signal.signal(signal.SIGINT, signal_handler)
-
-# Loop through each line in stdin
-for line in sys.stdin:
-    # Split the line by spaces
-    tokens = line.split()
-    # If the tokens are not empty
-    if tokens:
-        # Increment the line count
-        line_count += 1
-        # Try to get the status code and the file size from the last two tokens
-        try:
-            status_code = tokens[-2]
-            file_size = int(tokens[-1])
-            # Add the file size to the total size
-            total_size += file_size
-            # If the status code is in the dictionary, increment its count
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-        except Exception:
-            # Ignore any exception and continue
-            pass
-    # If the line count is a multiple of 10, print the statistics
-    if line_count % 10 == 0:
-        print_stats()
-
-# Print the statistics at the end of the loop
-print_stats()
+except KeyboardInterrupt:
+    print("File size: {:d}".format(file_size))
+    for key, value in sorted(status_tally.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
