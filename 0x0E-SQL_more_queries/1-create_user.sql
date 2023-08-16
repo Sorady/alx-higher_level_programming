3d4755fe47ce3669af845899f8f77063438163e7
Sorady <125706196+Sorady@users.noreply.github.com>
2023-08-16 21:27:35 +0300

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,10 +1,3 @@
--- This script creates the MySQL server user user_0d_1
-
--- Create the user user_0d_1 with all privileges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Flush privileges
-FLUSH PRIVILEGES;
+-- script to run msql commands
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO user_0d_1@localhost
