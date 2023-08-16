b54177400ebb89d0ed40f7b6ed645ac606436b22
Sorady <125706196+Sorady@users.noreply.github.com>
2023-08-16 20:35:09 +0300

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,12 +1,3 @@
-USE mysql;
-
-SELECT
-  CONCAT('Grants for ', user, '@', host) AS grants,
-  privilege_type
-FROM
-  user_privileges
-WHERE
-  user IN ('user_0d_1', 'user_0d_2')
-ORDER BY
-  user,
-  host;
+-- Lists all privileges of the users user_0d_1 and user_0d_2.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
