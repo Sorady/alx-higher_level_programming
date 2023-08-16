3716c5a0f3078f20cfddabdf79f39a930226feec
Sorady <125706196+Sorady@users.noreply.github.com>
2023-08-16 21:26:49 +0300

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,7 +1,3 @@
--- This script lists all privileges of MySQL users user_0d_1 and user_0d_2
-
--- List privileges for user_0d_1
+-- Lists all privileges of the users user_0d_1 and user_0d_2.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- List privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
